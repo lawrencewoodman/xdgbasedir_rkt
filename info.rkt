f55424f25a8e3183172b2bda017f69eafe6b6975
Lawrence Woodman <lwoodman@vlifesystems.com>
2013-08-04 08:04:12 +0100

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,13 @@
+#lang setup/infotab
+
+(define name "xdgbasedir")
+(define blurb
+  (list "Functions to work with the XDG Base Directory Specification"))
+(define homepage "https://github.com/LawrenceWoodman/xdgbasedir_rkt/")
+(define primary-file "main.rkt")
+(define categories '(io))
+(define repositories '("4.x"))
+(define required-core-version "5.3")
+(define release-notes
+  (list
+   '(ul (li "Initial release"))))
