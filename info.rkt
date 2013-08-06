bb366b9d7d154b125e6646545efb11edd919996b
Lawrence Woodman <lwoodman@vlifesystems.com>
2013-08-06 16:05:39 +0100

    Add todo list

--- info.rkt
+++ info.rkt
@@ -10,6 +10,7 @@
 (define categories '(io))
 (define repositories '("4.x"))
 (define required-core-version "5.3")
+(define compile-omit-paths '("private"))
 (define release-notes
   (list
    '(ul (li "Initial release"))))
