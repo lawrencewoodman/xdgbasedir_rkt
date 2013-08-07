0a35564d55dd995fb3fc252118cb696b82405c18
Lawrence Woodman <lwoodman@vlifesystems.com>
2013-08-07 07:53:21 +0100

    Make suitable for new pkg system

---
+++ xdgbasedir/info.rkt
@@ -0,0 +1,16 @@
+#lang setup/infotab
+
+(define name "xdgbasedir")
+(define blurb
+  (list "Functions to work with the XDG Base Directory Specification"))
+(define homepage "https://github.com/LawrenceWoodman/xdgbasedir_rkt/")
+(define version "0.1")
+(define primary-file "main.rkt")
+(define scribblings '(("scribblings/xdgbasedir.scrbl" (multi-page))))
+(define categories '(io))
+(define repositories '("4.x"))
+(define required-core-version "5.3")
+(define compile-omit-paths '("private"))
+(define release-notes
+  (list
+   '(ul (li "Initial release"))))
